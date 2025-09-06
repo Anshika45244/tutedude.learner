9389a208c8914cf471f1d21618ca932d8ecff8e9
Anshika Verma <anshikaverma6334@gmail.com>
2025-09-06 18:14:35 -0400

    Create assignement1.task2.py

---
+++ assignement1.task2.py
@@ -0,0 +1,8 @@
+#creating a personalized greeting
+#taking input from the user
+firstname= input("Enter your first name: ")
+lastname= input("Enter your last name: ")
+#creating full name
+fullname= firstname+" "+lastname
+#display
+print(f"\nHello,{fullname}!Welcome to the Python world.")
